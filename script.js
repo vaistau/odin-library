7d3d8b854856c4167416bc4b27478a11c076f630
Vaiva S <vaistaugaityte@gmail.com>
2022-01-24 08:59:37 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const container = document.querySelector('#container');
+
 let myLibrary = [];
 
 const book = {
@@ -5,16 +7,20 @@ const book = {
         return this.title + ' by ' + this.author + ', ' + this.pages + ' pages, ' + this.read
     }
 }
+const addBookToLibrary = function (newBook) {
+    myLibrary.push(newBook);
+}
+
 
 const newBook = Object.create(book);
 newBook.title = 'The Hobbid'
 newBook.author = 'Ballsack'
 newBook.pages = '420'
 newBook.read = 'No'
+addBookToLibrary(newBook);
 
-const container = document.querySelector('#container');
-container.innerHTML = newBook.title;
+container.innerHTML = myLibrary[0].info;
+console.log(newBook.title)
 
-const addBookToLibrary = function (newBook) {
-    myLibrary.push(newBook);
-}
+
+
